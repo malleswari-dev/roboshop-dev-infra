a79ca8336dcab654c45794aa72d815064e7aab3d
malleswari <maddikirimalleswari4@gmail.com>
2025-11-17 11:20:24 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -7,5 +7,5 @@ locals {
     terraform = "true"
   }
   database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
