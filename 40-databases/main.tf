f532f5dd9a9747d0a54d73b800735286b73d7ec2
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 12:44:20 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -167,6 +167,7 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "redis" {
@@ -175,6 +176,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq" {
@@ -183,6 +185,7 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -191,4 +194,5 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
