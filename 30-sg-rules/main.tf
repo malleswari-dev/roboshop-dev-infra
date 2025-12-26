8c1fb226b38ae504b30ad179c648bd5195c64653
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 11:47:23 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -65,3 +65,13 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   security_group_id = local.catalogue_sg_id
   
 }
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id = local.catalogue_sg_id 
+  security_group_id = local.mongodb_sg_id
+  
+}
