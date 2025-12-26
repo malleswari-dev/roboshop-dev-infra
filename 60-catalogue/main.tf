965e02ee55141199ba3b91defeda4cf99e1e2543
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 11:49:45 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -33,7 +33,7 @@ resource "terraform_data" "catalogue" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/catalogue.sh",
-        "sudo sh /tmp/catalogue.sh catalogue-${var.environment}"
+        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
     ]
 
   }
