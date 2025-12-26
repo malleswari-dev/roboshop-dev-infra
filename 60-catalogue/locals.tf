b685a89e3a9cbe70ef208f376deefbd0e46696b7
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 11:27:42 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -6,7 +6,7 @@ locals {
     environment = var.environment
     terraform = "true"
   }
-  private_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  
+
 }
