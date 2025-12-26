e21a488f07d70f19c8ff3c36e5d0026b8d526252
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 17:24:52 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -30,3 +30,17 @@ resource "aws_lb_listener" "front_end" {
     }
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}" # catalogue.backend-alb-dev-malleswari.fun
+  type    = "A"
+
+  alias {
+    # these are alb details , not our domain details
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+    
+  }
+}
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -4,3 +4,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z10173741Y5Y3INEWNGTN"
+}
+
+variable "domain_name" {
+  default = "malleswari.fun"
+}
