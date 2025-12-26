4f4ca7155bc5602ef26073f5ecb959d4ed3eb8a9
malleswari <maddikirimalleswari4@gmail.com>
2025-12-26 17:08:18 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -8,5 +8,6 @@ locals {
   }
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 
 }
