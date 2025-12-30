12d4a94784be03f192b8379b1e8e44d34cf239c1
malleswari <maddikirimalleswari4@gmail.com>
2025-12-30 16:53:36 +0530

    terraform ; git push origin ain git add . ; git commit -m terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -85,3 +85,12 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   security_group_id = local.catalogue_sg_id
   
 }
+
+resource "aws_security_group_rule" "frontend_alb_public" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Replace with your public IP address
+  security_group_id = local.frontend_alb_sg_id
+}
