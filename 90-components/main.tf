e68d00c91ab381579bad888c299aaff336db2ede
malleswari <maddikirimalleswari4@gmail.com>
2026-01-01 18:56:55 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -5,8 +5,8 @@
 
 # }
 
-module "components" {
-    for_each = var.components
+module "component" {
+    for_each = var.component
     source = "git::https://malleswari-dev/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
